965c5911c24e326627de5eb4fb2fdd3a49a6f638
manuelpira <manuelalonsod@gmail.com>
2022-03-12 01:04:03 -0300

    Primer script.

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Ubuntu 20.04
+
+# Install 
+# echo "Install"
+# sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
+# sudo apt update
+# sudo apt install pipewire libspa-0.2-bluetooth pipewire-audio-client-libraries
+
+# Systemctl
+echo "Systemctl"
+systemctl --user daemon-reload
+systemctl --user --now disable pulseaudio.service pulseaudio.socket
+systemctl --user --now enable pipewire pipewire-pulse
+systemctl --user mask pulseaudio
+systemctl --user --now enable pipewire-media-session.service
+systemctl --user restart pipewire
+pactl info
+
+# # Rollback
+# echo "Rollback"
+# systemctl --user unmask pulseaudio
+# systemctl --user --now enable pulseaudio.service pulseaudio.socket
+# pactl info
